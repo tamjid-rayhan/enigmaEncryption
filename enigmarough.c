42bd51b0a7208d35c43de783c923abd6ff8cca39
Tamjid Rayhan <tamjidexpress1@gmail.com>
2017-10-30 17:51:30 +0600

    code does not work except for rotor offset AAA. rotors are brought into one function

--- enigmarough.c
+++ enigmarough.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #define DEBUG true
 
-char initRotorPos[]={'a','a','a'};
+char initRotorPos[]={'b','b','b'};
 //basic alphabatic order
 char basicOrder[]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 // Enigma i Rotor iii BDFHJLCPRTXVZNYEIWGAKMUSQO
@@ -19,21 +19,18 @@ char reflector[]={'y','r','u','h','q','s','l','d','p','x','n','g','o','k','m','i
 void initializeRotors();
 void printArray(char *k);
 void shiftArrayOnePosition(char *k, int size);
-char rotorIII(char m);
-char rotorII(char m);
-char rotorI(char m);
+char rotorForward(char m);
+
 int main(){
 //printf("%d %d",'a','z');
 //Code for changing the array initializations according to rotor starting position
 initializeRotors();
+//encryption
 char m=getchar();
-//printf("  %c", m);
-char r3=rotorIII(m);
-printf("  %c", r3);
-char r2=rotorII(r3);
-printf("  %c", r2);
-char r1=rotorI(r2);
-printf("  %c", r1);
+
+char rf=rotorForward(m);
+printf("  %c", rf);
+
 return 0;
 }
 void shiftArrayOnePosition(char *k, int size) {
@@ -79,22 +76,20 @@ for(i=0;i<26;i++){
 }
 printf("\n");
 }
-char rotorIII(char m){
+char rotorForward(char m){
     char c;
+    //rightmost rotor
     int index=m-97;
     shiftArrayOnePosition(rotorIIIorder,26);
     c=rotorIIIorder[index];
-    return c;
-}
-char rotorII(char m){
-char c;
-int index=m-97;
-c=rotorIIorder[index-1];
-    return c;
-}
-char rotorI(char m){
-char c;
-int index=m-97;
-c=rotorIorder[index];
+printf("  %c", c);
+    //middle rotor
+    index=c-97;
+    c=rotorIIorder[index-1];
+printf("  %c", c);
+    //left rotor
+    index=c-97;
+    c=rotorIorder[index];
+printf("  %c", c);
     return c;
 }
